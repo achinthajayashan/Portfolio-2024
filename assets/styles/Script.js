a65b7c573689495ee4fa4ed759285f8ccbe4b643
achinthajayashan <achintha.uns.2018@gmail.com>
2024-01-02 21:14:52 +0530

    text animation

--- assets/styles/Script.js
+++ assets/styles/Script.js
@@ -1,34 +1,43 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const texts = ["fullstack developer", "video editor", "designer"];
-    let index = 0;
-    let charIndex = 0;
-    let currentText = "";
+"use strict";
 
-    function type() {
-        if (charIndex < texts[index].length) {
-            currentText += texts[index].charAt(charIndex);
-            document.getElementById("typing-text").innerText = currentText;
-            charIndex++;
-            setTimeout(type, 200); // Adjust typing speed here (in milliseconds)
-        } else {
-            setTimeout(erase, 2000);
-        }
-    }
+let words = document.querySelectorAll(".word");
 
-    function erase() {
-        if (charIndex > 0) {
-            currentText = texts[index].substring(0, charIndex - 1);
-            document.getElementById("typing-text").innerText = currentText;
-            charIndex--;
-            setTimeout(erase, 100); // Adjust erasing speed here (in milliseconds)
-        } else {
-            index++;
-            if (index >= texts.length) {
-                index = 0;
-            }
-            setTimeout(type, 1000); // Delay before typing next text
-        }
-    }
+words.forEach((word)=>{
+        let letters = word.textContent.split("");
+        word.textContent="";
+        letters.forEach((letter)=>{
+            let span = document.createElement("span");
+            span.textContent=letter;
+            span.className="letter";
+            word.append(span);
+        });
+    });
 
-    type(); // Initial call to start typing
-});
+let currentWordIndex=0;
+let maxWordIndex = words.length-1;
+words[currentWordIndex].style.opacity="1";
+
+let rotateText =() =>{
+    let currentWord =words[currentWordIndex];
+    let nextWord= currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter,i)=>{
+       setTimeout(()=>{
+          letter.className="letter out";
+
+       },i*80);
+    });
+
+    nextWord.style.opacity="1";
+    Array.from(nextWord.children).forEach((letter, i)=>{
+        letter.className="letter behind";
+        setTimeout(()=>{
+            letter.className="letter in";
+
+        },340+i*80);
+    });
+    currentWordIndex= currentWordIndex===maxWordIndex ? 0 :currentWordIndex +1;
+};
+
+rotateText();
+setInterval(rotateText,4000);
